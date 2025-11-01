b5cab2c163214cea965ebe801336a014c4b0ed1a
garabavlad <garaba.vlad@gmail.com>
2025-11-01 12:32:23 +0100

    wrong test name

--- Mediators.Tests/NotificationHandlers/TrackMessageNotificationHandlerTests.cs
+++ Mediators.Tests/NotificationHandlers/TrackMessageNotificationHandlerTests.cs
@@ -7,7 +7,7 @@ using NUnit.Framework;
 namespace Mediators.NotificationHandlers.Tests;
 
 [TestFixture]
-internal sealed class GetMessageCountHandlerTests
+internal sealed class TrackMessageNotificationHandlerTests
 {
     [Test]
     public async Task TrackMessageNotification_IncreasesCount()
